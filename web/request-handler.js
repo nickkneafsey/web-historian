2a06a16febef4dd9eec859eb3692df56c2b2e112
nickkneafsey <nicholaskneafsey@gmail.com>
2015-12-16 15:45:13 -0800

    Serve up html on a GET request

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,26 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelp = require('./http-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.method === 'GET'){
+    if(req.url === '/'){
+      fs.readFile(__dirname+'/public/index.html','utf8', function(err, content) {
+        if (err) throw err;
+        else {
+          // res.write(content);
+          res.writeHead(200, httpHelp.headers);
+          res.write(content);
+          res.end();
+        }
+      });
+    }
+    if (req.url === '/arglebargle') {
+      res.writeHead(404,httpHelp.headers);
+      res.end();
+    }
+  }
+  // res.end(archive.paths.list);
 };
